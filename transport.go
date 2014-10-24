8f4b1bf60e67dce415f3d14c3ea54391033e1d48
Francesc Campoy <campoy@github.com>
2014-10-23 18:10:44 -0700

    modified with adg's feedback

--- transport.go
+++ transport.go
@@ -16,37 +16,43 @@
 package httplog
 
 import (
+	"log"
 	"net/http"
 	"net/http/httputil"
 )
 
 // Transport logs all requests and responses at every RoundTrip using the
 // provided logging function.
-// The body of the requests and responses is logged only if LogBody is true.
 type Transport struct {
 	Transport http.RoundTripper
-	LogBody   bool
-	Logf      func(format string, vs ...interface{})
+	// Should the body of the requests and responses be logged.
+	LogBody bool
+	// If Logf is nil log.Printf will be used.
+	Logf func(format string, vs ...interface{})
 }
 
 // Transport satifies http.RoundTripper
 func (t Transport) RoundTrip(req *http.Request) (*http.Response, error) {
+	if t.Logf == nil {
+		t.Logf = log.Printf
+	}
 	if b, err := httputil.DumpRequest(req, t.LogBody); err != nil {
-		t.Logf("dump request: %v", err)
+		t.Logf("httplog: dump request: %v", err)
+		return nil, err
 	} else {
-		t.Logf("%s", b)
+		t.Logf("httplog: %s", b)
 	}
 
 	res, err := t.Transport.RoundTrip(req)
 	if err != nil {
-		t.Logf("roundtrip error: %v", err)
+		t.Logf("httplog: roundtrip error: %v", err)
 		return res, err
 	}
 
 	if b, err := httputil.DumpResponse(res, t.LogBody); err != nil {
-		t.Logf("dump response: %v", err)
+		t.Logf("httplog: dump response: %v", err)
 	} else {
-		t.Logf("%s", b)
+		t.Logf("httplog: %s", b)
 	}
 	return res, err
 }
